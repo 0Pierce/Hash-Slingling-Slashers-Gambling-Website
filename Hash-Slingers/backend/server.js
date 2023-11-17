fc176db547622b25ee9f44529bc951534639f05a
nlaprade <123032857+nlaprade@users.noreply.github.com>
2023-11-16 20:53:37 -0500

    Update server.js

--- Hash-Slingers/backend/server.js
+++ Hash-Slingers/backend/server.js
@@ -1,42 +1,25 @@
-
-const express = require('express')
+const express = require('express');
 const errorController = require('./controllers/error.controller');
 const authController = require('./controllers/auth.controller');
 
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
-const app = express()
+const app = express();
 
 const userController = require('./controllers/userController');
 
-
-
-mongoose.connect('mongodb+srv://hashslinger:qKrqjdz1NFQkDGT2@hash-slinging.qyqsxr4.mongodb.net/?retryWrites=true&w=majority', {
-    userNewUrlParser: true,
+mongoose.connect('mongodb+srv://hashslingers:qKrqjdz1NFQkDGT2@hash-slinging.qyqsxr4.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error(err));
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.error(err));
 
 app.use(express.json());
 
 app.use("/api", userRoutes);
 
-app.listen(5000, ()=>{console.log("Live on port 5000")} );
-
-
-
-=======
-const app = express()
-
-app.get("/api", (req,res)=>{
-    res.json({"users":["UserOne", "UserTwo", "UserThree"]})
-})
-
-
-app.listen(5000, ()=>{console.log("Live on port 5000")} );
->>>>>>> origin/main
+app.listen(5000, () => { console.log("Live on port 5000") });
 
 app.post('/auth/signin', authController.signIn);
 app.post('/auth/signout', authController.signOut);
