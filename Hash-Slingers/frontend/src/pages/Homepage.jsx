b27db3e1920f39018e065aa2da031bee1042164f
Pierce <123277032+0Pierce@users.noreply.github.com>
2023-11-23 23:32:07 -0500

    Adjustments

--- Hash-Slingers/frontend/src/pages/Homepage.jsx
+++ Hash-Slingers/frontend/src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header.jsx'
 import LandScreen from '../components/HomePageWelcome.jsx'
 import '../styles/HomePage.css'
-import Pyramid from "../assets/svgs/pyramid.svg"
+
 
 function Homepage() {
   return (
@@ -11,7 +11,7 @@ function Homepage() {
     <Header></Header>
     <div className="HomepageBody">
       <h1>HomePage</h1>
-      <img src={Pyramid} alt="" />
+      
 
     </div>
     </>
