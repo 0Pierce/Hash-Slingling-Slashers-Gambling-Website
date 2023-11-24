8de6ff53c0caf98579fcaeb2b5e38ae56d878577
Pierce <123277032+0Pierce@users.noreply.github.com>
2023-11-23 23:27:46 -0500

    Minor adjustments

--- Hash-Slingers/frontend/src/components/HomePageWelcome.jsx
+++ Hash-Slingers/frontend/src/components/HomePageWelcome.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/HomePageWelcome.css'
-import { ReactComponent as MySvg } from "../assets/svgs/circles.svg"
+import Pyramid from "../assets/svgs/pyramid.svg"
 function HomePageWelcome() {
   return (
     <>
     <div className="HPWelcomeBody">
+      <div className="landPyramid"> <img src={Pyramid} alt="" /></div>
    
       <section>
         <span></span>
--- Hash-Slingers/frontend/src/pages/Homepage.jsx
+++ Hash-Slingers/frontend/src/pages/Homepage.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Header from '../components/Header.jsx'
 import LandScreen from '../components/HomePageWelcome.jsx'
 import '../styles/HomePage.css'
+import Pyramid from "../assets/svgs/pyramid.svg"
+
 function Homepage() {
   return (
     <>
     <LandScreen></LandScreen>
     <Header></Header>
     <div className="HomepageBody">
-
+      <h1>HomePage</h1>
+      <img src={Pyramid} alt="" />
 
     </div>
     </>
