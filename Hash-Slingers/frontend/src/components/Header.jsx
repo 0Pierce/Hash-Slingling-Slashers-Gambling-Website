2740bbef0289e501740e5927291b7466536e6cd9
cjdjperalta <cjdjperalta@gmail.com>
2023-11-30 20:27:24 -0500

    added useNavigate to Header

--- Hash-Slingers/frontend/src/components/Header.jsx
+++ Hash-Slingers/frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 import '../styles/Header.css'
 import {React, useState, useEffect} from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import fundLogo from "../assets/svgs/fundIcon.svg"
 
 
@@ -9,7 +9,7 @@ import fundLogo from "../assets/svgs/fundIcon.svg"
 
 const Navigation  = () => {
   const [isLogged, setIsLogged] = useState(() => localStorage.getItem('isLogged') === 'true');
-  
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Update state
@@ -26,25 +26,22 @@ const Navigation  = () => {
     };
   }, []);
 
-  const logout = () => {
+  const handleSignOut = () => {
     console.log('Logged Out');
     localStorage.setItem('isLogged', false);
     setIsLogged(false);
-    window.location.reload();
+    navigate('/Login');
   };
 
   return (
-  <>
     <div className="navs">
         <ul>
             <li><Link to="/">Home</Link></li>
-            {isLogged ? (<li><Link to="/Games">Games</Link></li>)  : (<li><Link to="/Games"></Link></li> )}
-            {isLogged ? (<li><Link to="/AddFunds">Add Funds</Link></li>)  : (<li><Link to="/AddFunds"></Link></li> )}
-            {isLogged ? (<li><Link to="/Login" onClick={logout}>SignOut</Link></li>)  : (<li><Link to="/Login">Login</Link></li> )}
-            
+            {isLogged && (<li><Link to="/Games">Games</Link></li>)}
+            {isLogged && (<li><Link to="/AddFunds">Add Funds</Link></li>)}
+            {isLogged ? (<li><Link to="/Login" onClick={handleSignOut}>SignOut</Link></li>)  : (<li><Link to="/Login">Login</Link></li> )}   
         </ul>
       </div>
-  </>
   );
 };
 
