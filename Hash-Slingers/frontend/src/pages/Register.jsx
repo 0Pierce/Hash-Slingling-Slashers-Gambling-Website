b92876b1d76303454faa0154f2d6ab1919e703ed
cjdjperalta <cjdjperalta@gmail.com>
2023-11-30 19:52:20 -0500

    improved Register component

--- Hash-Slingers/frontend/src/pages/Register.jsx
+++ Hash-Slingers/frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import {React, useState} from 'react'
 import Header from '../components/Header.jsx'
 import '../styles/Register.css'
 import Pyramid from "../assets/svgs/pyramid.svg"
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
 
@@ -11,16 +11,15 @@ function Register() {
     const [password, setPassword] = useState('');
     const [fullname, setFullname] = useState('');
     const [dateOfBirth, setDateOfBirth] = useState('');
-
-  
+    const navigate = useNavigate();
 
     const createAccount = async () => {
-        if (fullname.length !== 0 && username.length !== 0 && password.length !== 0 && email.length !== 0 && dateOfBirth.length !==0) {
-          try {
-            const response = await fetch('/api/user', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
+      if (fullname && username && password && email && dateOfBirth) {
+        try {
+          const response = await fetch('/api/user', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
               },
               body: JSON.stringify({
                 username,
@@ -31,58 +30,55 @@ function Register() {
               }),
             });
       
-            if (response.ok) {
-              console.log('Account created successfully');
-              // Optionally, redirect to login page or handle success
-            } else {
-              console.log('Account creation failed');
-              alert('Failed to create account');
-            }
-          } catch (error) {
-            console.error('Error during account creation:', error);
+          if (response.ok) {
+            console.log('Account created successfully');
+            alert('Account created successfully! You will be redirected to login.');
+            navigate('/Login');
+            // Optionally, redirect to login page or handle success
+          } else {
+            console.log('Account creation failed');
+            alert('Failed to create account.');
           }
-        } else {
+        } catch (error) {
+            console.error('Error during account creation:', error);
+            alert('An error occured. Please try again.');
+        }
+      } else {
           console.log('Account creation failed');
           alert('Missing input');
-        }
-      };
+      }
+    };
 
-  return (
-    <>
-    <Header/>
-    <div className="regBody">
-    
-    <img src={Pyramid} alt=""/>
-    <div className="regPanel">
-        <h1>Register</h1>
-        <div className="regInput">
-        <form action="">
-            <div className="regFields">
-                <div className="names">
-                    <input type="text" placeholder='Full Name' id='fullname' onChange={e=>setFullname(e.target.value)}/>
+    return (
+      <>
+        <Header/>
+        <div className="regBody">
+          <img src={Pyramid} alt=""/>
+          <div className="regPanel">
+            <h1>Register</h1>
+            <div className="regInput">
+              <form onSubmit={(e) => e.preventDefault()}>
+                <div className="regFields">
+                  <div className="names">
+                    <input type="text" placeholder='Full Name' required onChange={e => setFullname(e.target.value)}/>
+                  </div>
+                  <input type="text" placeholder='Username' required onChange={e => setUsername(e.target.value)}/>
+                  <input type="email" placeholder='Email' required onChange={e => setEmail(e.target.value)}/>
+                  <input type="password" placeholder='Password' required onChange={e => setPassword(e.target.value)}/>
+                  <input type="date" placeholder='Date of Birth' required onChange={e => setDateOfBirth(e.target.value)}/>
                 </div>
-                
-                <input type="text" placeholder='Username' id='username' onChange={e=>setUsername(e.target.value)} />
-                <input type="email" placeholder='Email' id='email'  onChange={e=>setEmail(e.target.value)}/>
-                <input type="text" placeholder='Password' id='pass' onChange={e=>setPassword(e.target.value)}/>
-                <input type="text" placeholder='Date of Birth' id='dob' onChange={e=>setDateOfBirth(e.target.value)}/>
+                <div className="regBtn">
+                  <button type="submit" onClick={createAccount}>Submit</button>
+                </div>
+                <div className="regLogin">
+                  <Link to="/Login">Login</Link>
+                </div>
+              </form>
             </div>
-           
-            <div className="regBtn"><button type="button" value="Submit" onClick={createAccount}>Submit</button></div>
-            <div className="regLogin"><Link to="/Login">Login</Link></div>
-
-        </form>
-        
+          </div>
         </div>
-      
-    </div>
-
-    
-    </div>
-   
-    
-    </>
-  )
-}
+      </>
+    );
+    }
 
 export default Register
