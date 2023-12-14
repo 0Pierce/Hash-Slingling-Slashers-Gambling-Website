fe0bbbbdc1baac1694a47d5b4197ed9e04cca74e
Pierce <123277032+0Pierce@users.noreply.github.com>
2023-12-14 18:58:44 -0500

    kgmj

--- Hash-Slingers/backend/server.js
+++ Hash-Slingers/backend/server.js
@@ -91,6 +91,7 @@ const port = process.env.PORT || 5000;
 app
   .listen(port, () => {
     console.log(`Server running on port ${port}`);
+    console.log(__dirname);
   })
   .on("error", (err) => {
     console.error("Error occurred starting the server:", err);
