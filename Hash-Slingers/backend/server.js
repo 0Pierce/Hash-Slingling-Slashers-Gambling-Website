308234d8b8fae0a6d63a1c8234ba66ceb83dd6b8
Pierce <123277032+0Pierce@users.noreply.github.com>
2023-12-14 18:54:42 -0500

    testin

--- Hash-Slingers/backend/server.js
+++ Hash-Slingers/backend/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 // Serve static files from the React build directory
 app.use(express.static(path.join(__dirname, "./frontend/build")));
-
+console.log(__dirname);
 // Route to serve the React app for all paths
 //app.get("/", function (req, res) {
 //  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
