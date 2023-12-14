007266f01e443d1084f0f9b7f40874d16a66cef0
Pierce <123277032+0Pierce@users.noreply.github.com>
2023-12-14 16:30:23 -0500

    serverjs stuff

--- Hash-Slingers/backend/server.js
+++ Hash-Slingers/backend/server.js
@@ -16,7 +16,8 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(CURRENT_WORKING_DIR, "build/app")));
+//app.use(express.static(path.join(CURRENT_WORKING_DIR, "build/app")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 mongoose
   .connect(
