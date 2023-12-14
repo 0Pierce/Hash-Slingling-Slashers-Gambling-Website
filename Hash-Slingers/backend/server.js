0b13271e03d84c9d7b994c8db29a754f19147d84
Pierce <123277032+0Pierce@users.noreply.github.com>
2023-12-14 18:48:30 -0500

    g

--- Hash-Slingers/backend/server.js
+++ Hash-Slingers/backend/server.js
@@ -30,9 +30,9 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 // Route to serve the React app for all paths
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
-});
+//app.get("/", function (req, res) {
+//  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
+//});
 
 mongoose
   .connect(
